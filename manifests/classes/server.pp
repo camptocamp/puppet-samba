23176e7114374920907dc450f570021a1c789613
Kris Buytaert <sdog@inuits.be>
2011-04-20 17:30:57 +0200

    Added case to support multiple distos

--- manifests/classes/server.pp
+++ manifests/classes/server.pp
@@ -3,10 +3,31 @@ class samba::server inherits samba::common {
     ensure => installed,
   }
 
+#  Needs to be samba on da debian box apparently I modified this to smb "
+#case $operatingsystem {
+#    Debian,Ubuntu: { include apache::debian}
+#    RedHat,CentOS: { include apache::redhat}
+#    default: { notice "Unsupported operatingsystem ${operatingsystem}" }
+#  }
+
+#$lsbdistid ? {
+#      default => "/etc/httpd/conf.d/foreman.conf",
+#      "Ubuntu" => "/etc/apache2/conf.d/foreman.conf"
+#    },
+
   service { "samba":
     ensure  => running,
     pattern => smbd,
-    restart => "/etc/init.d/smb reload",
+    name => $operatingsystem ? { 
+	"Debian" =>  "samba",
+	"Ubuntu" =>  "samba",
+	"CentOS" => "smb" ,
+	"RedHat" => "smb" },
+    restart => $operatingsystem ? { 
+	"Ubuntu" => "/etc/init.d/samba reload",
+	"Debian" => "/etc/init.d/samba reload",
+	"CentOS" => "/etc/init.d/smb reload" ,
+	"RedHat" => "/etc/init.d/smb reload" },
     require => Package[samba],
   }
 
