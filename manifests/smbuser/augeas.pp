07f34417cb780e61a343121e0507e79e38875646
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-04-15 13:35:11 +0200

    Use file() function instead of fileserver

--- manifests/smbuser/augeas.pp
+++ manifests/smbuser/augeas.pp
@@ -1,6 +1,6 @@
 class samba::smbuser::augeas {
   augeas::lens { 'smbusers':
-    lens_source => 'puppet:///modules/samba/lenses/smbusers.aug',
-    test_source => 'puppet:///modules/samba/lenses/test_smbusers.aug',
+    lens_content => file('samba/lenses/smbusers.aug'),
+    test_content => file('samba/lenses/test_smbusers.aug'),
   }
 }
